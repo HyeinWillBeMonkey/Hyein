0f488beae65d3777c3b3f31f58dc724ca8a0b65d
hyein0112 <bhi12134@gmail.com>
2021-12-10 10:30:05 +0900

    [CREATE] MainTabBarView

---
+++ Tita-Clone/Tita-Clone/Views/MainTabBarView.swift
@@ -0,0 +1,54 @@
+//
+//  MainTabBarView.swift
+//  Tita-Clone
+//
+//  Created by 혜인 on 2021/12/10.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class MainTabBarView: UIView{
+    
+    //MARK: - property
+    var lineViewGradient: CAGradientLayer!
+    
+    lazy var allClassInquiryButton = UIButton().then{
+        $0.setImage(UIImage(named: "GOOUT_SelectedAllClassInquiryButtonImage"), for: .normal)
+    }
+    
+    lazy var addButton = UIButton().then{
+        $0.setImage(UIImage(named: "GOOUT_MyClassInquiryButtonImage"), for: .normal)
+    }
+    
+    lazy var myClassInquiryButton = UIButton().then{
+        $0.setImage(UIImage(named: "GOOUT_MyPageImage"), for: .normal)
+    }
+    
+    lazy var lineView = UIView().then{
+        $0.backgroundColor = .white
+    }
+    
+    //MARK: - lifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layoutSetting()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - layoutSetting
+    func layoutSetting(){
+        self.backgroundColor = .white
+        
+        self.addSubview(lineView)
+        self.addSubview(addButton)
+        self.addSubview(myClassInquiryButton)
+        self.addSubview(allClassInquiryButton)
+        
+    }
+}
