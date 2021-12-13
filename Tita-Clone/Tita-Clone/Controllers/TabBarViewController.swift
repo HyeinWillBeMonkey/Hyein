5d1191da57cf5418d5ffc67debb5174e4d89e07b
hyein0112 <bhi12134@gmail.com>
2021-12-13 22:43:30 +0900

    [UPDATE] tabBarItem SelectedImage

--- Tita-Clone/Tita-Clone/Controllers/TabBarViewController.swift
+++ Tita-Clone/Tita-Clone/Controllers/TabBarViewController.swift
@@ -18,22 +18,25 @@ class TabBarViewController: UITabBarController {
     
        private func setUpTabBar() {
            self.tabBar.tintColor = .black
-
         
            let mainViewController = UINavigationController(rootViewController: MainViewController())
            mainViewController.tabBarItem.image = UIImage(named: "Tita-homeBtn")
+           mainViewController.tabBarItem.selectedImage = UIImage(named: "Tita-SelectHomeBtn")
            mainViewController.tabBarItem.imageInsets = .init(top: 0, left: 0, bottom: -30, right: 0)
            
            let noticeViewController = UINavigationController(rootViewController: NoticeViewController())
            noticeViewController.tabBarItem.image = UIImage(named: "Tita-noticeBtn")
+           noticeViewController.tabBarItem.selectedImage = UIImage(named: "Tita-SelectNoticeBtn")
            noticeViewController.tabBarItem.imageInsets = .init(top: 0, left: 0, bottom: -30, right: 0)
 
            let alarmViewController = UINavigationController(rootViewController: AlarmViewController())
            alarmViewController.tabBarItem.image = UIImage(named: "Tita-alarmBtn")
+           alarmViewController.tabBarItem.selectedImage = UIImage(named: "Tita-SelectAlarmBtn")
            alarmViewController.tabBarItem.imageInsets = .init(top: 0, left: 0, bottom: -30, right: 0)
            
            let mypageViewController = UINavigationController(rootViewController: MypageViewController())
            mypageViewController.tabBarItem.image = UIImage(named: "Tita-mypageBtn")
+           mypageViewController.tabBarItem.selectedImage = UIImage(named: "Tita-SelectMypageBtn")
            mypageViewController.tabBarItem.imageInsets = .init(top: 0, left: 0, bottom: -30, right: 0)
            
            viewControllers = [mainViewController, noticeViewController, alarmViewController, mypageViewController]
