ce18d83709de7a3c04a70ad9aab6e4df52006b33
백혜인 <81304598+hyein0112@users.noreply.github.com>
2021-12-20 17:33:56 +0900

    Update Tita-Clone/Tita-Clone/Controllers/NoticeViewController.swift
    Co-authored-by: 노연주 <yjnoh1016@naver.com>

--- Tita-Clone/Tita-Clone/Controllers/NoticeViewController.swift
+++ Tita-Clone/Tita-Clone/Controllers/NoticeViewController.swift
@@ -98,29 +98,3 @@ class NoticeViewController: UIViewController , UITableViewDelegate, UITableViewD
     
 }
 
-//MARK: - Preview
-#if DEBUG
-import SwiftUI
-struct NoticeViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-    @available(iOS 13.0.0, *)
-    func makeUIViewController(context: Context) -> UIViewController{
-        NoticeViewController()
-    }
-}
-@available(iOS 13.0, *)
-struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            NoticeViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
-
